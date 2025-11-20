ad244fa77daaaa20abca6b79daafd690e7937349
sahithi2221 <sahithi2221@gmail.com>
2025-11-20 13:58:52 +0530

    Create Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,5 @@
-Class Test{
-  public static void main(String args[])
-  {
-      System.out.println("Hello");
-  }
-};
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
